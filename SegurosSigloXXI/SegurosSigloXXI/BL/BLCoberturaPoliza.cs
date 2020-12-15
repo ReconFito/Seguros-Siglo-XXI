561646262cd0368e3a0ea2439124c31e795d568f
DanielZamoraSalas <dgzamoras@gmail.com>
2020-12-15 11:16:16 -0600

    Coneccion a base de datos
    Procedimientos para utilizar la base

---
+++ SegurosSigloXXI/SegurosSigloXXI/BL/BLCoberturaPoliza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SegurosSigloXXI.BL
+{
+    public class BLCoberturaPoliza
+    {
+        polizassigloxxiEntities coberturaPoliza = new polizassigloxxiEntities();
+        #region Lista Coberturas de pólizas
+        public List<paCoberturaPolizaSelect_Result> ListaCoberturaPolizas(int idCobertura = -1)
+        {
+            var registros = coberturaPoliza.paCoberturaPolizaSelect(idCobertura).ToList();
+            return registros;
+        }
+        #endregion
+
+        #region Inserta Cobertura póliza
+        public bool InsertaCoberturaPoliza(string nombre, string descripcion, decimal porcentaje)
+        {
+            int estadoInsert = coberturaPoliza.paCoberturaPolizaInsert(nombre, descripcion, porcentaje);
+            return estadoInsert > 0;
+        }
+        #endregion
+
+        #region Modifica Cobertura póliza
+        public bool ModificaCoberturaPoliza(int idCobertura, string nombre, string descripcion, decimal porcentaje)
+        {
+            int estadoUpdate = coberturaPoliza.paCoberturaPolizaUpdate(idCobertura, nombre, descripcion, porcentaje);
+            return estadoUpdate > 0;
+        }
+        #endregion
+
+        #region Elimina Cobertura póliza
+        public bool EliminaCoberturaPoliza(int idCobertura)
+        {
+            int estadoDelete = coberturaPoliza.paCoberturaPolizaDelete(idCobertura);
+            return estadoDelete > 0;
+        }
+        #endregion
+    }
+}
---
+++ SegurosSigloXXI/SegurosSigloXXI/BL/BLMantenimientoClientes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SegurosSigloXXI.BL
+{
+    public class BLMantenimientoClientes
+    {
+        // Instancia modelo de bases de datos
+        polizassigloxxiEntities mClientes = new polizassigloxxiEntities();
+
+        #region Listar Clientes
+        public List<paMantenimientoClienteSelect_Result> ListaClientes(Nullable<int> _cedula = null)
+        {
+            var listaClientes = mClientes.paMantenimientoClienteSelect(_cedula).ToList();
+            return listaClientes;
+        }
+        #endregion
+
+        #region Insertar Cliente
+        public bool InsertaCliente(int cedula, string genero,
+                                   DateTime fechaNaci, string nombre, string primerApellido,
+                                   string direccion, int tel1,
+                                   string correo, string segundoApellido = null,
+                                   Nullable<int> tel2 = null)
+        {
+            int registro = mClientes.paMantenimientoClienteInsert(cedula, genero, fechaNaci,
+                                                                nombre, primerApellido, segundoApellido,
+                                                                direccion, tel1, tel2, correo);
+            return registro > 0;
+        }
+        #endregion
+
+        #region Modificar Cliente
+        public bool ModificaCliente(int idCliente, int cedula, string genero,
+                                   DateTime fechaNaci, string nombre, string primerApellido,
+                                   string direccion, int tel1,
+                                   string correo, string segundoApellido,
+                                   Nullable<int> tel2 = null)
+        {
+            int estado = mClientes.paMantenimientoClienteUpdate(idCliente, cedula, genero, fechaNaci,
+                                                                nombre, primerApellido, segundoApellido,
+                                                                direccion, tel1, tel2.Value, correo);
+            return estado > 0;
+        }
+        #endregion
+
+        #region Elimina Cliente
+        public bool EliminaCliente(int _cedula)
+        {
+            int estado = mClientes.paMantenimientoClienteDelete(_cedula);
+            return estado > 0;
+        }
+        #endregion
+    }
+}
